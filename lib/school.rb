e2636df1684d24df40e6a5dd339826961129f7a5
AHMED AFANDY <eng.computer1@yahoo.com>
2020-03-14 22:48:26 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+
+attr_accessor :name , :roster
+
+def initialize(name)
+   @name=name
+   @roster={}
+end
+
+#def roster=(roster)
+  #@roster ={}
+#end
+#def roster
+  #@roster
+#end
+
+def add_student(student_name, grade)
+  roster[grade] ||= []   # ||= if roster[grade] already  has  a value leave it not change ,if not equal it to empty array =[]
+  roster[grade] << student_name
+end
+
+def grade(grade)
+   roster[grade]
+end
+
+
+def sort
+   roster.each do |grade, student_name|
+      roster[grade] = student_name.sort
+   end
+end
+
+end
