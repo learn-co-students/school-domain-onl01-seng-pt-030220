1a98b0dc4e1282f9249d33dee90082cb3753af91
Alejandra I. Lee Vargas <leevargasale@gmail.com>
2020-03-18 01:08:15 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+require 'pry'
+
+class School
+
+  def initialize(name)
+    @name = name
+    self.roster= {}
+  end
+  
+  def add_student=(student, grade)
+    @student = student
+    @grade = grade
+    self.roster["@#{grade}"] = []
+    binding.pry
+    self.roster["@#{grade}"] << "@#{student}"
+  end
+  
+ def add_student(student, grade)
+   @student
+   @grade
+ end
+ 
+def roster=(roster)
+  @roster = roster
+end
+ 
+ def roster
+   @roster
+ end
+ 
+end
